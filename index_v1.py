e3450a82018556435c48931a62e89469099f4fca
Avery Khoo <averykhoo@gmail.com>
2024-04-10 21:08:57 +0800

    refactor

--- index_v1.py
+++ index_v1.py
@@ -3,8 +3,16 @@ from dataclasses import dataclass
 from dataclasses import field
 
 
-class AutoRemoveCounter(Counter):
+class MultiSet(Counter):
+    """
+    poor man's multiset, based off a Counter with some added safeguards
+    also it deletes the key when the value goes down to zero to save a bit of ram
+    """
     def __setitem__(self, key, value):
+        if not isinstance(value, int):
+            raise TypeError(f'value {value} is not an integer')
+        if value < 0:
+            raise ValueError(f'value {value} is negative')re
         if value == 0:
             super().__delitem__(key)
         else:
@@ -14,7 +22,7 @@ class AutoRemoveCounter(Counter):
 @dataclass(frozen=True)
 class GraphIndex:
     nodes: list = field(default_factory=list)
-    edges: AutoRemoveCounter = field(default_factory=AutoRemoveCounter)
+    edges: MultiSet = field(default_factory=MultiSet)
     index_nodes_from: dict = field(default_factory=dict)
     index_nodes_to: dict = field(default_factory=dict)
-    index_counter: AutoRemoveCounter = field(default_factory=AutoRemoveCounter)
+    index_counter: MultiSet = field(default_factory=MultiSet)
