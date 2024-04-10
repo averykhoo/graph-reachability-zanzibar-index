4aed84f5a4b3b82cd5b72954810bd525681aaefb
Avery Khoo <averykhoo@gmail.com>
2024-04-10 12:42:34 +0800

    gotta name these things

--- index_v1.py
+++ index_v1.py
@@ -1,11 +1,20 @@
+from collections import Counter
 from dataclasses import dataclass
 from dataclasses import field
+
+
+class AutoRemoveCounter(Counter):
+    def __setitem__(self, key, value):
+        if value == 0:
+            super().__delitem__(key)
+        else:
+            super().__setitem__(key, value)
 
 
 @dataclass(frozen=True)
 class GraphIndex:
     nodes: list = field(default_factory=list)
-    edges: list = field(default_factory=list)
+    edges: AutoRemoveCounter = field(default_factory=AutoRemoveCounter)
     index_nodes_from: dict = field(default_factory=dict)
     index_nodes_to: dict = field(default_factory=dict)
-    index_counter: dict = field(default_factory=dict)
+    index_counter: AutoRemoveCounter = field(default_factory=AutoRemoveCounter)
