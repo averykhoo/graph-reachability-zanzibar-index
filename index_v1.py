137435e84aa38342b016706b6dfffb529bb736e3
khoo.ys <khoo.ys@xtraman.org>
2024-04-11 16:44:10 +0800

    refactor

--- index_v1.py
+++ index_v1.py
@@ -35,120 +35,116 @@ class MultiSet(Counter):
 
 @dataclass(frozen=True)
 class AcyclicGraphReachabilityIndex:
+    direct_edge_counts: MultiSet[tuple[str, str], int] = field(default_factory=MultiSet)  # {(source, dest): count}
+    
+    # index for "indirect" edges, which include direct edges
     index_paths: dict[str, set[str]] = field(default_factory=dict)  # {source: {dest, ...}}
     inverted_index_paths: dict[str, set[str]] = field(default_factory=dict)  # {dest: {source, ...}}
     index_path_counts: MultiSet[tuple[str, str], int] = field(default_factory=MultiSet)  # {(source, dest): count}
-    edge_counts: MultiSet[tuple[str, str], int] = field(default_factory=MultiSet)  # {(source, dest): count}
+
+    def _check(self):
+        for node_from in self.index_paths:
+            for node_to in self.index_paths[node_from]:
+                assert node_to in self.inverted_index_paths
+                assert node_from in self.inverted_index_paths[node_to]
+                assert self.index_path_counts[(node_from, node_to)] > 0
+
+    def _reachable_backwards(self, _node):
+        reachable_backwards = MultiSet()
+        for reachable_node in self.inverted_index_paths.get(_node, set()):
+            reachable_backwards[reachable_node] = self.index_path_counts[(reachable_node, _node)]
+        assert reachable_backwards[_node] == 0
+        return reachable_backwards
+
+    def _reachable_forwards(self, _node):
+        reachable_forwards = MultiSet()
+        for reachable_node in self.index_paths.get(_node, set()):
+            reachable_forwards[reachable_node] = self.index_path_counts[(_node, reachable_node)]
+        assert reachable_forwards[_node] == 0
+        return reachable_forwards
+
+    def _add_indirect_edge(self, _from, _to, _add_count):
+        self._check()
+        self.index_path_counts[(_from, _to)] += _add_count
+        if self.index_path_counts[(_from, _to)]:
+            self.index_paths.setdefault(_from, set()).add(_to)
+            self.inverted_index_paths.setdefault(_to, set()).add(_from)
+        else:
+            if _to in self.index_paths[_from]:
+                self.index_paths[_from].remove(_to)
+                if not self.index_paths[_from]:
+                    del self.index_paths[_from]
+            if _from in self.inverted_index_paths[_to]:
+                self.inverted_index_paths[_to].remove(_from)
+                if not self.inverted_index_paths[_to]:
+                    del self.inverted_index_paths[_to]
+        self._check()
 
     def add_edge(self, node_from, node_to):
         # sanity check
+        self._check()
         assert node_from != node_to
 
         # ensure acyclic invariant holds
         if self.index_path_counts[(node_to, node_from)] > 0:
             raise ValueError(f'{node_from=} is reachable from {node_to=}, adding this edge would create a cycle')
 
-        # get the nodes that can reach node_from
-        reachable_from_node_from = MultiSet()
-        for node in self.inverted_index_paths.get(node_from, set()):
-            reachable_from_node_from[node] = self.index_path_counts[(node, node_from)]
-        assert reachable_from_node_from[node_from] == 0, reachable_from_node_from
+        # get the reachable nodes and check invariants
+        reachable_from_node_from = self._reachable_backwards(node_from)
+        reachable_from_node_to = self._reachable_forwards(node_to)
         assert reachable_from_node_from[node_to] == 0, reachable_from_node_from
-
-        # get the nodes that are reachable from node_to
-        reachable_from_node_to = MultiSet()
-        for node in self.index_paths.get(node_to, set()):
-            reachable_from_node_to[node] = self.index_path_counts[(node_to, node)]
         assert reachable_from_node_to[node_from] == 0, reachable_from_node_to
-        assert reachable_from_node_to[node_to] == 0, reachable_from_node_to
 
         # add the indirect edges:
         for from_node, from_count in reachable_from_node_from.items():
             for to_node, to_count in reachable_from_node_to.items():
-                self.index_path_counts[(from_node, to_node)] += from_count * to_count
-                self.index_paths.setdefault(from_node, set()).add(to_node)
-                self.inverted_index_paths.setdefault(to_node, set()).add(from_node)
+                self._add_indirect_edge(from_node, to_node, from_count * to_count)
 
         # add the node_to's reachable nodes to node_from
         for to_node, count in reachable_from_node_to.items():
-            self.index_path_counts[(node_from, to_node)] += count
-            self.index_paths.setdefault(node_from, set()).add(to_node)
-            self.inverted_index_paths.setdefault(to_node, set()).add(node_from)
+            self._add_indirect_edge(node_from, to_node, count)
 
         # make node_to reachable from all the nodes that can reach node_from
         for from_node, count in reachable_from_node_from.items():
-            self.index_path_counts[(from_node, node_to)] += count
-            self.index_paths.setdefault(from_node, set()).add(node_to)
-            self.inverted_index_paths.setdefault(node_to, set()).add(from_node)
+            self._add_indirect_edge(from_node, node_to, count)
 
         # add the direct edge
-        self.index_path_counts[(node_from, node_to)] += 1
-        self.index_paths.setdefault(node_from, set()).add(node_to)
-        self.inverted_index_paths.setdefault(node_to, set()).add(node_from)
-        self.edge_counts[(node_from, node_to)] += 1
+        self._add_indirect_edge(node_from, node_to, 1)
+        self.direct_edge_counts[(node_from, node_to)] += 1
+        self._check()
 
     def remove_edge(self, node_from, node_to):
         # sanity check
+        self._check()
         assert node_from != node_to
 
         # ensure there's an edge to remove
-        if self.edge_counts[(node_from, node_to)] == 0:
+        if self.direct_edge_counts[(node_from, node_to)] == 0:
             raise ValueError(f'{node_from=} has no direct edge to {node_to=}, cannot remove nonexistent edge')
 
-        # get the nodes that can reach node_from
-        reachable_from_node_from = MultiSet()
-        for node in self.inverted_index_paths.get(node_from, set()):
-            reachable_from_node_from[node] = self.index_path_counts[(node, node_from)]
-        assert reachable_from_node_from[node_from] == 0, reachable_from_node_from
+        # get the reachable nodes and check invariants
+        reachable_from_node_from = self._reachable_backwards(node_from)
+        reachable_from_node_to = self._reachable_forwards(node_to)
         assert reachable_from_node_from[node_to] == 0, reachable_from_node_from
-
-        # get the nodes that are reachable from node_to
-        reachable_from_node_to = MultiSet()
-        for node in self.index_paths.get(node_to, set()):
-            reachable_from_node_to[node] = self.index_path_counts[(node_to, node)]
         assert reachable_from_node_to[node_from] == 0, reachable_from_node_to
-        assert reachable_from_node_to[node_to] == 0, reachable_from_node_to
 
         # remove the indirect edges:
         for from_node, from_count in reachable_from_node_from.items():
             for to_node, to_count in reachable_from_node_to.items():
-                self.index_path_counts[(from_node, to_node)] -= from_count * to_count
-                if self.index_path_counts[(from_node, to_node)] == 0:
-                    self.index_paths[from_node].remove(to_node)
-                    self.inverted_index_paths[to_node].remove(from_node)
+                self._add_indirect_edge(from_node, to_node, -(from_count * to_count))
 
         # remove the node_to's reachable nodes from node_from
         for to_node, count in reachable_from_node_to.items():
-            self.index_path_counts[(node_from, to_node)] -= count
-            if self.index_path_counts[(node_from, to_node)] == 0:
-                self.index_paths[node_from].remove(to_node)
-                if not self.index_paths[node_from]:
-                    del self.index_paths[node_from]
-                self.inverted_index_paths[to_node].remove(node_from)
-                if not self.inverted_index_paths[to_node]:
-                    del self.inverted_index_paths[to_node]
+            self._add_indirect_edge(node_from, to_node, -count)
 
         # make node_to less reachable from all the nodes that can reach node_from
         for from_node, count in reachable_from_node_from.items():
-            self.index_path_counts[(from_node, node_to)] -= count
-            if self.index_path_counts[(from_node, node_to)] == 0:
-                self.index_paths[from_node].remove(node_to)
-                if not self.index_paths[from_node]:
-                    del self.index_paths[from_node]
-                self.inverted_index_paths[node_to].remove(from_node)
-                if not self.inverted_index_paths[node_to]:
-                    del self.inverted_index_paths[node_to]
+            self._add_indirect_edge(from_node, node_to, -count)
 
         # remove the direct edge
-        self.index_path_counts[(node_from, node_to)] -= 1
-        if self.index_path_counts[(node_from, node_to)] == 0:
-            self.index_paths[node_from].remove(node_to)
-            if not self.index_paths[node_from]:
-                del self.index_paths[node_from]
-            self.inverted_index_paths[node_to].remove(node_from)
-            if not self.inverted_index_paths[node_to]:
-                del self.inverted_index_paths[node_to]
-        self.edge_counts[(node_from, node_to)] -= 1
+        self._add_indirect_edge(node_from, node_to, -1)
+        self.direct_edge_counts[(node_from, node_to)] -= 1
+        self._check()
 
 
 if __name__ == '__main__':
@@ -161,4 +157,4 @@ if __name__ == '__main__':
     print(idx.index_paths)
     print(idx.inverted_index_paths)
     print(idx.index_path_counts)
-    print(idx.edge_counts)
+    print(idx.direct_edge_counts)
