14e491ed9c69f8e3157b7831c3287a4ab8e50921
Avery Khoo <averykhoo@gmail.com>
2024-05-08 11:47:35 +0800

    nodes and edges instead

--- index_v3.py
+++ index_v3.py
@@ -13,6 +13,10 @@ class Node(SQLModel, table=True):
     implicit: bool = Field(default=True)  # if implicit, should be auto-deleted once all referencing tuples are deleted
     reference_count: int = Field(default=0)
 
+    @property
+    def predicate_or_ellipsis(self) -> str | EllipsisType:
+        return Ellipsis if self.predicate == '...' else self.predicate
+
 
 class Edge(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
@@ -28,42 +32,34 @@ engine = create_engine('sqlite:///database.db', echo=True)
 SQLModel.metadata.create_all(engine)
 
 
-def _add_db_edges_unsafe(subject_predicate: str | EllipsisType | None,
-                         subject_id: int | None,
-                         relation: str | None,
+def _add_db_edges_unsafe(subject_id: int | None,
                          object_id: int | None,
                          direct_edge_count: int | None,
                          indirect_edge_count: int | None,
                          ):
     assert indirect_edge_count != 0
-    assert subject_predicate != relation or subject_id != object_id
+    assert subject_id != object_id
 
     with Session(engine) as session:
-        _select = select(RelationalTriple)
-        if subject_predicate is not None:
-            _select = _select.where(RelationalTriple.subject_predicate == subject_predicate)
+        _select = select(Edge)
         if subject_id is not None:
-            _select = _select.where(RelationalTriple.subject_id == subject_id)
-        if relation is not None:
-            _select = _select.where(RelationalTriple.relation == relation)
+            _select = _select.where(Edge.subject_id == subject_id)
         if object_id is not None:
-            _select = _select.where(RelationalTriple.object_id == object_id)
+            _select = _select.where(Edge.object_id == object_id)
 
         triples = session.exec(_select).all()
         if not triples:
             if not direct_edge_count and not indirect_edge_count:  # both are zero or None
                 return
-            if subject_predicate is None or subject_id is None:
+            if subject_id is None:
                 return
-            if relation is None or object_id is None:
+            if object_id is None:
                 return
-            session.add(RelationalTriple(subject_predicate=subject_predicate,
-                                         subject_id=subject_id,
-                                         relation=relation,
-                                         object_id=object_id,
-                                         direct_edge_count=direct_edge_count or 0,
-                                         indirect_edge_count=indirect_edge_count or 0,
-                                         ))
+            session.add(Edge(subject_id=subject_id,
+                             object_id=object_id,
+                             direct_edge_count=direct_edge_count or 0,
+                             indirect_edge_count=indirect_edge_count or 0,
+                             ))
             return
 
         for triple in triples:
@@ -84,9 +80,7 @@ def _add_db_edges_unsafe(subject_predicate: str | EllipsisType | None,
             session.add(triple)
 
 
-def _add_edge_unsafe(subject_predicate: str | EllipsisType | None,
-                     subject_id: int | None,
-                     relation: str | None,
+def _add_edge_unsafe(subject_id: int | None,
                      object_id: int | None,
                      multiplier: int | None,
                      ):
@@ -94,13 +88,13 @@ def _add_edge_unsafe(subject_predicate: str | EllipsisType | None,
     assert multiplier in {-1, 1}
 
     # we need to remove direct edge first to preserve the invariant
-    if (subject_predicate != relation or subject_id != object_id) and multiplier < 0:
-        _add_db_edges_unsafe(subject_predicate, subject_id, relation, object_id, multiplier, multiplier)
+    if subject_id != object_id and multiplier < 0:
+        _add_db_edges_unsafe(subject_id, object_id, multiplier, multiplier)
     else:
         raise NotImplementedError  # disable this for now because i need to remove the whole entity, not just a node
 
     # alternatively, remove the entire node from direct edges
-    if subject_predicate == relation and subject_id == object_id:
+    if subject_id == object_id:
         assert multiplier == -1
         _add_db_edges_unsafe(None, subject_id, None, None, None, 0)
         _add_db_edges_unsafe(None, None, None, object_id, None, 0)
@@ -109,77 +103,71 @@ def _add_edge_unsafe(subject_predicate: str | EllipsisType | None,
     reachable_from_node_from = MultiSet()
     reachable_from_node_to = MultiSet()
     with Session(engine) as session:
-        triples_from = session.exec(select(RelationalTriple)
-                                    .where(RelationalTriple.relation == subject_predicate)
-                                    .where(RelationalTriple.object_id == subject_id)
+        triples_from = session.exec(select(Edge)
+                                    .where(Edge.object_id == subject_id)
                                     ).all()
         for triple in triples_from:
-            reachable_from_node_from[triple.subject_predicate, triple.subject_id] = triple.indirect_edge_count
-        triples_to = session.exec(select(RelationalTriple)
-                                  .where(RelationalTriple.subject_predicate == relation)
-                                  .where(RelationalTriple.subject_id == object_id)
+            reachable_from_node_from[triple.subject_id] = triple.indirect_edge_count
+        triples_to = session.exec(select(Edge)
+                                  .where(Edge.subject_id == object_id)
                                   ).all()
         for triple in triples_to:
-            reachable_from_node_to[triple.relation, triple.object_id] = triple.indirect_edge_count
+            reachable_from_node_to[triple.object_id] = triple.indirect_edge_count
 
     # ensure we're not creating a cycle
-    assert reachable_from_node_from[relation, object_id] == 0, reachable_from_node_from
-    assert reachable_from_node_to[subject_predicate, subject_id] == 0, reachable_from_node_to
+    assert reachable_from_node_from[object_id] == 0, reachable_from_node_from
+    assert reachable_from_node_to[subject_id] == 0, reachable_from_node_to
 
     # add the indirect edges:
     for (_subject_predicate, _subject_id), from_count in reachable_from_node_from.items():
         for (_relation, _object_id), to_count in reachable_from_node_to.items():
-            _add_edge_unsafe(_subject_predicate, _subject_id, _relation, _object_id, from_count * to_count * multiplier)
+            _add_edge_unsafe(_subject_id, _object_id, from_count * to_count * multiplier)
 
     # add the node_to's reachable nodes to node_from
     for (_subject_id, _relation), count in reachable_from_node_to.items():
-        _add_edge_unsafe(subject_predicate, subject_id, _subject_id, _relation, count * multiplier)
+        _add_edge_unsafe(subject_id, _subject_id, count * multiplier)
 
     # make node_to reachable from all the nodes that can reach node_from
     for (_subject_predicate, _subject_id), count in reachable_from_node_from.items():
-        _add_edge_unsafe(_subject_predicate, _subject_id, relation, object_id, count * multiplier)
+        _add_edge_unsafe(_subject_id, object_id, count * multiplier)
 
     # add the direct edge last to preserve the invariant
-    if (subject_predicate != relation or subject_id != object_id) and multiplier > 0:
-        _add_db_edges_unsafe(subject_predicate, subject_id, relation, object_id, multiplier, multiplier)
+    if (subject_id != object_id) and multiplier > 0:
+        _add_db_edges_unsafe(subject_id, object_id, multiplier, multiplier)
 
 
-def add_edge(subject_predicate, subject_id, relation, object_id):
+def add_edge(subject_id, object_id):
     # sanity check
-    assert subject_predicate != relation or subject_id != object_id
+    assert subject_id != object_id
 
     with Session(engine) as session:
-        triple = session.exec(select(RelationalTriple)
-                              .where(RelationalTriple.subject_predicate == relation)
-                              .where(RelationalTriple.subject_id == object_id)
-                              .where(RelationalTriple.relation == subject_predicate)
-                              .where(RelationalTriple.object_id == subject_id)
+        triple = session.exec(select(Edge)
+                              .where(Edge.subject_id == object_id)
+                              .where(Edge.object_id == subject_id)
                               ).first()
         # ensure acyclic invariant holds
         if triple is not None and triple.indirect_edge_count > 0:
-            raise ValueError(f'{subject_predicate=} {subject_id=} is reachable from {relation=} {object_id=}, '
+            raise ValueError(f'{subject_id=} is reachable from {object_id=}, '
                              f'adding this edge would create a cycle')
 
-    _add_edge_unsafe(subject_predicate, subject_id, relation, object_id, 1)
+    _add_edge_unsafe(subject_id, object_id, 1)
 
 
-def remove_edge(subject_predicate, subject_id, relation, object_id):
+def remove_edge(subject_id, object_id):
     # sanity check
-    assert subject_predicate != relation or subject_id != object_id
+    assert subject_id != object_id
 
     with Session(engine) as session:
-        triple = session.exec(select(RelationalTriple)
-                              .where(RelationalTriple.subject_predicate == subject_predicate)
-                              .where(RelationalTriple.subject_id == subject_id)
-                              .where(RelationalTriple.relation == relation)
-                              .where(RelationalTriple.object_id == object_id)
+        triple = session.exec(select(Edge)
+                              .where(Edge.subject_id == subject_id)
+                              .where(Edge.object_id == object_id)
                               ).first()
         # ensure acyclic invariant holds
         if triple is None or triple.direct_edge_count == 0:
-            raise ValueError(f'{subject_predicate=} {subject_id=} has no direct edge to {relation=} {object_id=}, '
+            raise ValueError(f'{subject_id=} has no direct edge to {object_id=}, '
                              f'cannot remove nonexistent edge')
 
-    _add_edge_unsafe(subject_predicate, subject_id, relation, object_id, -1)
+    _add_edge_unsafe(subject_id, object_id, -1)
 
 # def remove_node(node: Node):
 #     _add_edge_unsafe(node, node, -1)
